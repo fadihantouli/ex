cf514cea1176579c5b1477a01dc0e77b911eb31e
qusailafi <qusailafi2@gmail.com>
2026-03-26 23:22:11 +0300

    code latest

--- android/app/src/main/kotlin/com/example/asss/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.asss
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,91 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:xvisa/controller/country_and_city/cubit.dart';
+import 'package:xvisa/controller/languag_cubit/loce_cubit.dart';
+import 'package:xvisa/controller/languag_cubit/loce_state.dart';
+import 'package:xvisa/controller/profile_cubit/cubit.dart';
+import 'package:xvisa/utills/app_theme/app_theme.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'controller/force_update.dart';
+import 'shared/local/cach_helper.dart';
+import 'shared/remote/dio.dart';
+import 'view/splash_screen/splash_screen.dart';
+import '../../generated/l10n.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ForceUpdateController forceUpdateController=Get.put(ForceUpdateController());
+
+  await CacheHelper.init();
+  await DioHelper.initail();
+  await ScreenUtil.ensureScreenSize();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  final designSize = const Size(300, 800);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LanguageCubit()..getSavedLanguage(),),
+        BlocProvider(create: (context) => CountryAndCityCubit()..getCountry()..getCity(),),
+        BlocProvider(create: (context) => ProfileCubit())
+      ],
+      child: BlocBuilder<LanguageCubit, languageStates>(
+        builder: (context, state) {
+          print(CacheHelper.getString('token'));
+          print(CacheHelper.getString('LOCALE'));
+          if (state is changeLanguageState) {
+            return ScreenUtilInit(
+              designSize: designSize,
+              fontSizeResolver: (fontSize, instance) {
+                final display = View.of(context).display;
+                final screenSize = display.size / display.devicePixelRatio;
+                final scaleWidth = screenSize.width / designSize.width;
+                return fontSize * scaleWidth;
+              },
+              minTextAdapt: true,
+              splitScreenMode: true,
+              builder: (context, child) {
+                return MaterialApp(
+                    theme: appTheme(),
+                  builder: (context, child) => ResponsiveBreakpoints.builder(
+                    breakpoints: [
+                      const Breakpoint(start: 0, end: 450, name: MOBILE),
+                      const Breakpoint(start: 451, end: 800, name: TABLET),
+                      const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                      const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+                    ],
+                    child: child!,
+                  ),
+                  localizationsDelegates: const [
+                    S.delegate,
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                  ],
+                  //qusai
+                  supportedLocales: [    Locale('en'),
+                    Locale('ar'),],
+                  locale: state.locale,
+                    title: "ex visa",
+                    debugShowCheckedModeBanner: false,
+                    home: const SplashScreen(),
+                );
+              },
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
